2e1d8d4241fbe7c1f35819d7f5c28491395dd93a
Anton-Dudko <60478086+Anton-Dudko@users.noreply.github.com>
2021-11-23 00:53:42 +0300

    second commit

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,17 @@
-import javax.security.auth.Subject;
+import by.epam.lab.Material;
+import by.epam.lab.Subject;
 
 public class Runner {
     public static void main(String[] args) {
+        Material steel = new Material("Steel", 7850);
+        Subject wire = new Subject("Wire", steel, 0.03);
+        System.out.printf(wire.toString() + "\n");
 
+        Material copper = new Material("Copper", 8500);
+
+        wire.setMaterial(copper);
+
+        System.out.printf("The wire mass: " + wire.getMass() + "\n");
     }
 
 }
